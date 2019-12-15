97f93872536c984c0eb45e1770e40f0937463912
J. Luz <up201703782@fe.up.pt>
2019-12-15 19:55:58 +0000

    Finished application
    Co-authored-by: diogoalex01 <up201706892@fe.up.pt>
    Co-authored-by: joaohenriqueluz <up201703782@fe.up.pt>

--- TP2/src/application.c
+++ TP2/src/application.c
@@ -7,14 +7,13 @@ char server_response[3];
 
 int main(int argc, char **argv)
 {
-	int socket_fd, ret;
+	int socket_fd, socket_client, ret;
 	struct sockaddr_in server_addr;
-	// int bytes;
 
 	if (argc != 2)
 	{
-		printf("Wrong number of arguments.\n");
-		printf("Usage: %s ftp://[<user>:<password>@]<host>/<url-path>\n");
+		printf("     **Wrong number of arguments**\n");
+		printf("\tUsage: %s ftp://[<user>:<password>@]<host>/<url-path>\n", argv[0]);
 		return 1;
 	}
 
@@ -23,88 +22,71 @@ int main(int argc, char **argv)
 
 	char *server_ip = get_IP(url_arg.host);
 
-	/*server address handling*/
+	/* server address handling */
 	bzero((char *)&server_addr, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = inet_addr(server_ip); /* 32 bit Internet address network byte ordered */
 	server_addr.sin_port = htons(SERVER_PORT);			/* server TCP port must be network byte ordered */
 
-	/*open an TCP socket*/
+	/* open an TCP socket */
 	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		perror("socket()");
+		perror("     **Unable to open TCP socket.**\n\n");
 		exit(0);
 	}
 
-	/*connect to the server*/
+	/* connect to the server */
 	if (connect(socket_fd,
 				(struct sockaddr *)&server_addr,
 				sizeof(server_addr)) < 0)
 	{
-		perror("connect()");
+		perror("     **Unable to connect to the server.**\n\n");
 		exit(0);
 	}
 
-	ret = get_reply(server_response, 1024, socket_fd);
+	ret = get_reply(server_response, socket_fd);
+
 	if (ret == CONNECTED)
 	{
-		printf("Connection Established\n");
+		printf("   [Connection Established]\n");
 	}
 
 	if (strlen(url_arg.user))
 	{
 		if (login(url_arg.user, url_arg.password, socket_fd))
 		{
-			printf("Invalid login\n");
+			printf("     **Invalid login**\n\n");
 			return 1;
 		}
 	}
 
 	if (set_passive_mode(socket_fd))
 	{
-		printf("Could not set passive mode\n");
+		printf("     **Could not set passive mode**\n\n");
 		return 1;
 	}
 
-	if (download(socket_fd))
+	if (download(socket_fd, &socket_client))
 	{
-		printf("Could not download\n");
+		printf("     **Could not download**\n\n");
 		return 1;
 	}
 
-	// /*send a string to the server*/
-	// bytes = write(socket_fd, buf, strlen(buf));
-	// printf("Bytes escritos %d\n", bytes);
+	if (disconnect_sockets(socket_fd, socket_client))
+		return 1;
 
-	// close(socket_fd);
-
-	exit(0);
+	return 0;
 }
 
 char *get_IP(const char *name)
 {
 	struct hostent *h;
 
-	/*
-	struct hostent
-	{
-		char *h_name; Official name of the host.char **h_aliases; A NULL-terminated array of alternate names for the host. 
-		int h_addrtype; The type of address being returned; usually AF_INET.
-		int h_length; The length of the address in bytes.
-		char **h_addr_list;	A zero-terminated array of network addresses for the host. Host addresses are in Network Byte Order.
-	};
-
-	#define h_addr h_addr_list[0] The first address in h_addr_list.
-	*/
-
 	if ((h = gethostbyname(name)) == NULL)
 	{
-		herror("gethostbyname");
+		herror("        **Unable to get host name.**");
 		exit(1);
 	}
-
-	printf("Host name  : %s\n", h->h_name);
-	printf("IP Address : %s\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
 
 	return inet_ntoa(*((struct in_addr *)h->h_addr));
 }
@@ -120,7 +102,7 @@ int parse_arguments(char *arguments)
 	memcpy(url_arg.server, arguments, 6);
 	if (strcmp(url_arg.server, "ftp://"))
 	{
-		printf("Expected 'ftp://', got '%s'\n", url_arg.server);
+		printf("\n     **Expected 'ftp://' host, got '%s'**\n\n", url_arg.server);
 		return 1;
 	}
 
@@ -155,7 +137,7 @@ int parse_arguments(char *arguments)
 			strcpy(url_arg.password, credentials);
 			credentials = strtok(NULL, ":");
 		}
-		// parse host and url-path - host/url-path
+		// parse host and url-path - host/folder/filename
 		tk = strtok(url, "/");
 		strcpy(url_arg.host, tk);
 
@@ -169,6 +151,8 @@ int parse_arguments(char *arguments)
 		}
 
 		memcpy(url_arg.url, &url_arg.url[1], strlen(url_arg.url));
+		strcpy(url_arg.filename, strchr(url_arg.url, '/'));
+		memcpy(url_arg.filename, &url_arg.filename[1], strlen(url_arg.filename));
 	}
 	// in case there's no login info
 	else
@@ -188,7 +172,8 @@ int parse_arguments(char *arguments)
 		}
 
 		memcpy(url_arg.url, &url_arg.url[1], strlen(url_arg.url));
-		url_arg.url[strlen(url_arg.url)] = '\0';
+		strcpy(url_arg.filename, strchr(url_arg.url, '/'));
+		memcpy(url_arg.filename, &url_arg.filename[1], strlen(url_arg.filename));
 	}
 
 	print_parsed_url();
@@ -198,24 +183,24 @@ int parse_arguments(char *arguments)
 
 void print_parsed_url()
 {
-	printf(" Server: %s\n User: %s\n Password: %s\n Host: %s\n URL: 9%s99\n",
-		   url_arg.server, url_arg.user, url_arg.password, url_arg.host, url_arg.url);
+	printf("\n   [Information parsed]\n    - Server: %s\n    - User: %s\n    - Password: %s\n    - Host: %s\n    - URL: %s\n    - Filename: %s\n",
+		   url_arg.server, url_arg.user, url_arg.password, url_arg.host, url_arg.url, url_arg.filename);
 }
 
-static int get_reply(char *resp, int len, int socket_fd)
+static int get_reply(char *resp, int socket_fd)
 {
 	usleep(100 * 1000);
-	memset(resp, 0, 1024);
-	read(socket_fd, resp, 1024);
+	memset(resp, 0, MAX_BUFFER_SIZE);
+	read(socket_fd, resp, MAX_BUFFER_SIZE);
 	char *asn_server = strndup(resp, 3);
-	printf("respond:%s\n", resp);
+	//printf("respond:%s\n", resp);
 	return atoi(asn_server);
 }
 
 int login(char *user, char *password, int socket_fd)
 {
-	char user_cmd[1024];
-	char password_cmd[1024];
+	char user_cmd[MAX_BUFFER_SIZE];
+	char password_cmd[MAX_BUFFER_SIZE];
 	int ret;
 	// send USER
 	sprintf(user_cmd, "USER %s\r\n", user);
@@ -223,16 +208,16 @@ int login(char *user, char *password, int socket_fd)
 
 	if (!ret)
 	{
-		printf("Could not send username\n");
+		printf("     **Could not send username**\n");
 		close(socket_fd);
 		return 1;
 	}
 
-	ret = get_reply(server_response, 1024, socket_fd);
+	ret = get_reply(server_response, socket_fd);
 
 	if (ret != REQ_PASSWORD)
 	{
-		printf("Invalid username\n");
+		printf("     **Invalid username**\n");
 		close(socket_fd);
 		return 1;
 	}
@@ -243,41 +228,41 @@ int login(char *user, char *password, int socket_fd)
 
 	if (!ret)
 	{
-		printf("Could not send password\n");
+		printf("     **Could not send password**\n");
 		close(socket_fd);
 		return 1;
 	}
 
-	ret = get_reply(server_response, 1024, socket_fd);
+	ret = get_reply(server_response, socket_fd);
 
 	if (ret != CORRECT_PASSWORD)
 	{
-		printf("Invalid password\n");
+		printf("     **Invalid password**\n");
 		close(socket_fd);
 		return 1;
 	}
 
-	printf("User authenticated\n");
+	printf("   [User authenticated]\n");
 	return 0;
 }
 
-int set_passive_mode(socket_fd)
+int set_passive_mode(int socket_fd)
 {
 	int ret, a, b, c, d, pa, pb;
 	ret = write(socket_fd, PASSIVE_CMD, strlen(PASSIVE_CMD));
 
 	if (!ret)
 	{
-		printf("Could not send passive mode request\n");
+		printf("     **Could not send passive mode request**\n");
 		close(socket_fd);
 		return 1;
 	}
 
-	ret = get_reply(server_response, 1024, socket_fd);
+	ret = get_reply(server_response, socket_fd);
 
 	if (ret != PASSIVE_MODE)
 	{
-		printf("Passive mode not enabled\n");
+		printf("     **Passive mode not enabled**\n");
 		close(socket_fd);
 		return 1;
 	}
@@ -286,35 +271,26 @@ int set_passive_mode(socket_fd)
 	reply = strrchr(server_response, '(');
 	sscanf(reply, "(%d,%d,%d,%d,%d,%d)", &a, &b, &c, &d, &pa, &pb);
 	sprintf(url_arg.ipaddr, "%d.%d.%d.%d", a, b, c, d);
-	url_arg.port = pa * 256 + pb;
-	printf("addr %s\n", url_arg.ipaddr);
-	printf("port %d\n", url_arg.port);
-
-	printf("Passive mode enabled\n");
+	url_arg.port = pa * MAX_BUFFER_SIZE / 4 + pb;
+	printf("   [Passive mode enabled]\n");
 	return 0;
 }
 
 int send_retr(int socket_fd)
 {
-	char retr_cmd[1024];
+	char retr_cmd[MAX_BUFFER_SIZE];
 	int ret;
-	printf("\n2.1\n");
-	sprintf(retr_cmd, "RETR /parrot/last-sync.txt\r\n");
-	printf("\n2.2\n");
 
+	sprintf(retr_cmd, "RETR %s\r\n", url_arg.url);
 	ret = write(socket_fd, retr_cmd, strlen(retr_cmd));
-	printf("\n2.3\n");
 
 	if (!ret)
 	{
 		close(socket_fd);
 		return 1;
 	}
-	printf("\n2.4\n");
 
-	ret = get_reply(server_response, 1024, socket_fd);
-	printf("\n2.5\n");
-	printf("%d", ret);
+	ret = get_reply(server_response, socket_fd);
 
 	if (ret != RETR)
 	{
@@ -322,21 +298,20 @@ int send_retr(int socket_fd)
 		return 1;
 	}
 
-	printf("RETR sent\n");
+	printf("   [Sending RETR command]\n");
 	return 0;
 }
 
-int download(int socket_fd)
+int download(int socket_fd, int *socket_client)
 {
-	int socket_client;
 	struct sockaddr_in server_addr_client;
 	FILE *new_file;
-	char buffer[1024];
+	char buffer[MAX_BUFFER_SIZE];
 	int ret;
 
-	if (!(new_file = fopen(url_arg.url, "w")))
+	if (!(new_file = fopen(url_arg.filename, "w")))
 	{
-		printf("Unable to open file %s\n", url_arg.url);
+		printf("     **Unable to open file %s**\n", url_arg.filename);
 		return 1;
 	}
 
@@ -347,53 +322,62 @@ int download(int socket_fd)
 	server_addr_client.sin_port = htons(url_arg.port);				/*server TCP port must be network byte ordered */
 
 	/*open an TCP socket*/
-	if ((socket_client = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	if (((*socket_client) = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		perror("socket()");
+		perror("     **Unable to open TCP socket**\n");
 		exit(0);
 	}
 	/*connect to the server*/
-	printf("\n1\n");
-
-	if (connect(socket_client, (struct sockaddr *)&server_addr_client, sizeof(server_addr_client)) < 0)
+	if (connect((*socket_client), (struct sockaddr *)&server_addr_client, sizeof(server_addr_client)) < 0)
 	{
-		perror("connect()");
+		perror("     **Unable connect to the server**\n");
 		exit(0);
 	}
-	printf("\n2\n");
 
 	if (send_retr(socket_fd))
 	{
-		printf("Unable to send RETR\n");
+		printf("     **Unable to send RETR**\n");
 		return 1;
 	}
-	printf("\n3\n");
-	while ((ret = read(socket_client, buffer, 1024)))
+
+	while ((ret = read((*socket_client), buffer, MAX_BUFFER_SIZE)))
 	{
 		if ((ret = fwrite(buffer, ret, 1, new_file)) < 0)
 		{
-			printf("Unable to write to file %s\n");
+			printf("     **Unable to write to file %s**\n", url_arg.filename);
 			return 1;
 		}
 	}
-	printf("\n4\n");
+
 	if (ret < 0)
 	{
-		printf("Unable to read from socket\n");
+		printf("     **Unable to read from socket**\n");
 		return 1;
 	}
-	printf("\n5\n");
-	/*ret = get_reply(server_response, 1024, socket_fd);
-	printf("\n6\n");
+
+	/*
+	ret = get_reply(server_response, socket_fd);
+
 	if (ret != DOWNLOAD_COMPLETED)
-	{	
+	{
 
 		close(socket_client);
 		return 1;
 	}*/
+
 	fclose(new_file);
-	printf("Download completed\n");
+	printf("   [Download completed]\n");
 	return 0;
 }
-// ftp://host/url-path
-// ftp://user:password@host/url-path
+
+int disconnect_sockets(int socket_fd, int socket_client)
+{
+	if (close(socket_fd) || close(socket_client))
+	{
+		printf("     **Error closing sockets**\n");
+		return 1;
+	}
+
+	printf("   [Sockets disconnected]\n\n");
+	return 0;
+}
--- TP2/src/application.h
+++ TP2/src/application.h
@@ -16,54 +16,98 @@
 #define SERVER_PORT 21
 
 // status codes
-#define RETR 150 // Files are in good condition and ready to open data connection.
-#define CONNECTED 220 // The service is ready to execute the new user's request.
+#define RETR 150               // Files are in good condition and ready to open data connection.
+#define CONNECTED 220          // The service is ready to execute the new user's request.
 #define DOWNLOAD_COMPLETED 226 // Download was sucessful.
-#define PASSIVE_MODE 227 // Entering passive mode.
-#define CORRECT_PASSWORD 230 // Users logged in to continue.
-#define REQ_PASSWORD 331 // The user name is correct and a password is required.
-#define WRONG_PASSWORD 530 // Is not logged in.
+#define PASSIVE_MODE 227       // Entering passive mode.
+#define CORRECT_PASSWORD 230   // Authentication completed.
+#define REQ_PASSWORD 331       // The user name is correct and a password is required.
+#define WRONG_PASSWORD 530     // Authentication failed.
 
 #define PASSIVE_CMD "PASV\r\n"
+#define MAX_BUFFER_SIZE 1024
+#define IP_ADDRESS_SIZE 32
 
 typedef struct
 {
-    char user[255];
-    char password[255];
-    char host[255];
-    char url[255];
-    char server[255];
-    char ipaddr[32];
+    char user[MAX_BUFFER_SIZE / 4];
+    char password[MAX_BUFFER_SIZE / 4];
+    char host[MAX_BUFFER_SIZE / 4];
+    char url[MAX_BUFFER_SIZE / 4];
+    char filename[MAX_BUFFER_SIZE / 4];
+    char server[MAX_BUFFER_SIZE / 4];
+    char ipaddr[IP_ADDRESS_SIZE];
     int port;
 } url_syntax;
 
 /**
- * @brief 
+ * @brief Gets the ip address by the host name
  * 
- * @param name 
+ * @param name Name of the host
  */
 char *get_IP(const char *name);
 
 /**
- * @brief 
+ * @brief Parses aguments
  * 
- * @param arguments 
- * @return int 
+ * @param arguments Arguments given
+ * @return int Returns 0 on success, 1 otherwise
  */
 int parse_arguments(char *arguments);
 
 /**
- * @brief 
- * 
+ * @brief Prints to screen the information parsed 
  */
 void print_parsed_url();
 
-static int get_reply(char *resp, int len, int socket_fd);
+/**
+ * @brief Gets the reply object from the server
+ * 
+ * @param resp 
+ * @param socket_fd File descriptor for server TCP socket
+ * @return int Returns 0 on success, 1 otherwise
+ */
+static int get_reply(char *resp, int socket_fd);
 
-int login(char* user,char* password, int socket_fd);
+/**
+ * @brief Sends login information to server
+ * 
+ * @param user User name credential
+ * @param password Password credential
+ * @param socket_fd File descriptor for server TCP socket
+ * @return int Returns 0 on success, 1 otherwise
+ */
+int login(char *user, char *password, int socket_fd);
 
-int set_passive_mode(socket_fd);
+/**
+ * @brief Sets the passive mode object
+ * 
+ * @param socket_fd File descriptor for server TCP socket
+ * @return int Returns 0 on success, 1 otherwise
+ */
+int set_passive_mode(int socket_fd);
 
+/**
+ * @brief Sends RETR command to server
+ * 
+ * @param socket_fd File descriptor for server TCP socket
+ * @return int Returns 0 on success, 1 otherwise
+ */
 int send_retr(int socket_fd);
 
-int download(int socket_fd);
+/**
+ * @brief Downloads the requested file
+ * 
+ * @param socket_fd File descriptor for server TCP socket
+ * @return int Returns 0 on success, 1 otherwise
+ */
+int download(int socket_fd, int *socket_client);
+
+/**
+ * @brief Disconnects the used TCP sockets
+ * 
+ * @param socket_fd File descriptor for server TCP socket
+ * @param socket_client File descriptor for client TCP socket
+ * @return int Returns 0 on success, 1 otherwise
+ */
+int disconnect_sockets(int socket_fd, int socket_client);
